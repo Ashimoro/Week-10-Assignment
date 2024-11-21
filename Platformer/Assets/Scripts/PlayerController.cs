64b77dd8fc69970f1a22748e24cce79e8db84043
Ashimoro <fl34un@gmail.com>
2024-11-20 21:06:40 -0500

    Times up

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
+
+
 {
+    public Rigidbody2D rb;
+
+    private bool moving;
+    public float jumpForce = 1500f;
+
     public enum FacingDirection
     {
         left, right
@@ -12,30 +23,64 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
-        //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+ 
+        float hInput = Input.GetAxis("Horizontal");
+        float speed = 100 * Time.deltaTime;
+        Vector2 playerInput = new Vector2(hInput * speed, 0);
         MovementUpdate(playerInput);
+
+        if (hInput >= 0.01 || hInput <= -0.01)
+        {
+            moving = true;
+        } else
+        {
+            moving = false;
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+
+        //Debug.Log(rb.velocity);
+    }
+
+
+    public void Jump()
+    {
+
+            rb.AddForce (Vector2.up * jumpForce, ForceMode2D.Force);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        rb.MovePosition(rb.position + playerInput);
     }
+
 
     public bool IsWalking()
     {
+        if (moving == true)
+        {
+            return true;
+        }
         return false;
     }
     public bool IsGrounded()
     {
-        return true;
+        if (rb.velocity == Vector2.zero)
+        {
+            return true;
+        }
+        return false;
+        
     }
 
     public FacingDirection GetFacingDirection()
@@ -43,3 +88,4 @@ public class PlayerController : MonoBehaviour
         return FacingDirection.left;
     }
 }
+
